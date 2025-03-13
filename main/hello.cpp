f1190ff2cafd65b960a23fbbe9f45a652b7aebcf
Akshat Singh Jaswal <sja.akshat@gmail.com>
2025-03-13 13:25:18 +0530

    webhook added

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
+  cout << "Hello, Webhook\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
